51933b93e4be2ee84c4cfbec13c66cc332704e78
deuterotheodore <d.a.bach@yandex.com>
2025-06-24 17:53:35 +0200

    Update sample_strategies.py
    removed linear_weights

--- sample_strategies.py
+++ sample_strategies.py
@@ -3,7 +3,6 @@ import numpy as np
 #current agent genome: all values are float16
 #PD strategy:
 # cooperation_bias (float): constant collaboration preference, dominates first move
-# #linear_weights (8-dim): currently disabled, will be removed 
 # remote_weights (2x2 symmetric matrix): weights on own and opponent cooperation average over full game (long-term memory) 
 # recent_weights (6x6 symmetric matrix): weights on own and opponent cooperation/defection over past 3 turns 
 # contrition (float): biases weight of own past defection vs cooperation (high contrition means defections are weighed more strongly, depending on weight sign this may still influence behavior either way). Value 1.0 means symmetric weight on cooperation/defection
@@ -18,12 +17,11 @@ import numpy as np
 # mating_display (float): extra energy expended by males in order to increase chance of being chosen by females (bonus to energy as perceived by female choice algorithm)
 # chivalry (float): constant cooperation bonus applied only by males interacting with females
 # ladys_choice (4-dim): four weights for female mate choice, values [strength, fitness, caring, kinship]
-#total 12 items undergoing mutation-selection (excluding linear_weights): 8 float, 5-dim, 4-dim, 2x2, 6x6: total 57 float16, of which 16 redundant (symmetric matrices).
+#total 12 items undergoing mutation-selection: 8 float, 5-dim, 4-dim, 2x2, 6x6: total 57 float16, of which 16 redundant (symmetric matrices).
 
 sample_strategies = {
     "AC": {
         "cooperation_bias": np.float16(5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -38,7 +36,6 @@ sample_strategies = {
     },
     "AD": {
         "cooperation_bias": np.float16(-5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -52,14 +49,13 @@ sample_strategies = {
         "bigotry": np.float16(1.0)
     },
     "TFT": {
-        "cooperation_bias": np.float16(0.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
+        "cooperation_bias": np.float16(5.0),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 5, 0, 0],
+            [0, 0, 0, 9, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0]
         ], dtype=np.float16),
@@ -67,68 +63,49 @@ sample_strategies = {
         "bigotry": np.float16(1.0)
     },
     "TFTT": {
-        "cooperation_bias": np.float16(10.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
+        "cooperation_bias": np.float16(4.0),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 5, 0],
-            [0, 0, 0, 5, 0, 0],
+            [0, 0, 0, 0.3, 9, 0],
+            [0, 0, 0, 9, 0.2, 0],
             [0, 0, 0, 0, 0, 0]
         ], dtype=np.float16),
         "contrition": np.float16(1.0),
         "bigotry": np.float16(2.0)
     },
     "TFTTT": {
-        "cooperation_bias": np.float16(5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
+        "cooperation_bias": np.float16(4.0),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 2, 0, 0],
-            [0, 0, 0, 0, 2, 0],
-            [0, 0, 0, 0, 0, 2]
+            [0, 0, 0, 5.5, 5.5, 5.5],
+            [0, 0, 0, 5.5, 5.5, 5.5],
+            [0, 0, 0, 5.5, 5.5, 5.5]
         ], dtype=np.float16),
         "contrition": np.float16(1.0),
-        "bigotry": np.float16(1.0)
+        "bigotry": np.float16(0.4)
     },
     "GT": {
         "cooperation_bias": np.float16(5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
-        "remote_weights": np.array([[-5, 0], [0, 0]], dtype=np.float16),
+        "remote_weights": np.array([[5, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, -5, 0, 0],
-            [0, 0, 0, 0, -5, 0],
-            [0, 0, 0, 0, 0, -5]
+            [0, 0, 0, 5, 0, 0],
+            [0, 0, 0, 0, 5, 0],
+            [0, 0, 0, 0, 0, 5]
         ], dtype=np.float16),
         "contrition": np.float16(1.0),
         "bigotry": np.float16(2.0)
     },
-    "TFTT-Fair1": {
-        "cooperation_bias": np.float16(20.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
-        "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
-        "recent_weights": np.array([
-            [2, 0, 0, 15, 15, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [15, 0, 0, 0, 5, 0],
-            [15, 0, 0, 5, 0, 0],
-            [0, 0, 0, 0, 0, 0]
-        ], dtype=np.float16),
-        "contrition": np.float16(0.5),
-        "bigotry": np.float16(2.0)
-    },
-    "TFTT-Fair2": {
+    "TFTT-Fair": {
         "cooperation_bias": np.float16(5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [-2, 0, 0, 0, 0, 0],
@@ -141,39 +118,22 @@ sample_strategies = {
         "contrition": np.float16(0.5),
         "bigotry": np.float16(1.5)
     },
-    "TFTT-Fair3": {
-        "cooperation_bias": np.float16(5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
-        "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
-        "recent_weights": np.array([
-            [-3, 0, 0, 0, 0, 0],
-            [0, -3, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0]
-        ], dtype=np.float16),
-        "contrition": np.float16(0.5),
-        "bigotry": np.float16(1.5)
-    },
     "Pavlov": {
-        "cooperation_bias": np.float16(0.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
+        "cooperation_bias": np.float16(4.5),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
-            [0, 0, 0, -5, 0, 0],
+            [15, 0, 0, -10, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
-            [-5, 0, 0, 0, 0, 0],
+            [-10, 0, 0, 6, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0]
         ], dtype=np.float16),
         "contrition": np.float16(1.0),
-        "bigotry": np.float16(1.0)
+        "bigotry": np.float16(2.0)
     },
     "FTFT": {
         "cooperation_bias": np.float16(2.5),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -188,28 +148,12 @@ sample_strategies = {
     },
     "random": {
         "cooperation_bias": np.float16(0.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0]
-        ], dtype=np.float16),
-        "contrition": np.float16(1.0),
-        "bigotry": np.float16(1.0)
-    },
-    "STFT": {
-        "cooperation_bias": np.float16(-5.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
-        "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
-        "recent_weights": np.array([
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 5, 0, 0],
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0]
         ], dtype=np.float16),
@@ -218,7 +162,6 @@ sample_strategies = {
     },
     "GTFT": {
         "cooperation_bias": np.float16(2.5),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -231,9 +174,8 @@ sample_strategies = {
         "contrition": np.float16(1.0),
         "bigotry": np.float16(0.5)
     },
-    "TC": {
+    "TC": { #"trusting cooperator"
         "cooperation_bias": np.float16(0.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[5, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -246,9 +188,8 @@ sample_strategies = {
         "contrition": np.float16(1.0),
         "bigotry": np.float16(1.0)
     },
-    "MT": {
+    "MT": { #"mutual trust"
         "cooperation_bias": np.float16(0.0),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[2.5, 0], [0, 2.5]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -261,9 +202,8 @@ sample_strategies = {
         "contrition": np.float16(1.0),
         "bigotry": np.float16(1.0)
     },
-    "TBV": {
+    "TBV": { #"trust-but-verify"
         "cooperation_bias": np.float16(2.5),
-        "linear_weights": np.zeros(8, dtype=np.float16),
         "remote_weights": np.array([[2.5, 0], [0, 0]], dtype=np.float16),
         "recent_weights": np.array([
             [0, 0, 0, 0, 0, 0],
@@ -275,6 +215,48 @@ sample_strategies = {
         ], dtype=np.float16),
         "contrition": np.float16(1.0),
         "bigotry": np.float16(1.0)
+    },
+    "Claude": {
+        "cooperation_bias": np.float16(1.0),
+        "remote_weights": np.array([[0, 0], [0, 0]], dtype=np.float16),
+        "recent_weights": np.array([
+            [ 3.0,  0.0,  0.0, -3.0, -2.0,  0.0],
+            [ 0.0,  2.0,  0.0, -2.0, -1.0,  0.0],
+            [ 0.0,  0.0,  1.0, -1.0, -1.0,  0.0],
+            [-3.0, -2.0, -1.0,  3.0,  3.0,  0.0],
+            [-2.0, -1.0, -1.0,  3.0,  3.0,  0.0],
+            [ 0.0,  0.0,  0.0,  0.0,  0.0,  1.0]
+        ], dtype=np.float16),
+        "contrition": np.float16(1.0),
+        "bigotry": np.float16(2.0)
+    },
+    "fair": {
+        "cooperation_bias": np.float16(3.0),
+        "remote_weights": np.array([[5, 2], [2, 10]], dtype=np.float16),
+        "recent_weights": np.array([
+            [ 4.0,  0.0,  0.0, -2.0, -2.0,  0.0],
+            [ 0.0,  3.0,  0.0, -1.5, -1.5,  0.0],
+            [ 0.0,  0.0,  0.0, -0.2, 0.0,  0.0],
+            [-2.0, -1.5, -0.2,  2.5,  4.0,  0.0],
+            [-2.0, -1.5, 0.0,  4.0,  1.5,  0.0],
+            [ 0.0,  0.0,  0.0,  0.0,  0.0,  0.5]
+        ], dtype=np.float16),
+        "contrition": np.float16(1.0),
+        "bigotry": np.float16(1.2)
+    },
+    "evolved": {
+        "cooperation_bias": np.float16(4.35),
+        "remote_weights": np.array([[6.63, 2.47], [2.47, 10.28]], dtype=np.float16),
+        "recent_weights": np.array([
+            [ 4.58,  -0.64, -0.97,-1.96, -2.85, 0.84],
+            [ -0.64,  3.12, -0.26, -1.18, -2.07,  0.24],
+            [ -0.97, -0.26,  0.07, 0.51, -1.3, 0.21],
+            [-1.96, -1.18, 0.51,  2.1,  3.3,  0.03],
+            [-2.85, -2.07, -1.3,  3.3,  1.41, -0.28],
+            [ 0.84,  0.24, -0.21, 0.03, -0.28,  0.07]
+        ], dtype=np.float16),
+        "contrition": np.float16(0.89),
+        "bigotry": np.float16(1.34)
     }
 }
 
